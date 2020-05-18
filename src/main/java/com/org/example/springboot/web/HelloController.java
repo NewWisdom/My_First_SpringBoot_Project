2b02005f3147545c77d23474141588eb8a815020
wlgp2500 <wlgp2500@naver.com>
2020-05-18 14:34:45 +0900

    ch02 테스트 코드 작성

--- src/main/java/com/org/example/springboot/web/HelloController.java
+++ src/main/java/com/org/example/springboot/web/HelloController.java
@@ -1,18 +1,22 @@
 package com.org.example.springboot.web;
+
 import com.org.example.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //
+@RestController
 public class HelloController {
 
-    @GetMapping("/hello") //
-    public String hello(){
+    @GetMapping("/hello")
+    public String hello() {
         return "hello";
     }
     @GetMapping("/hello/dto")
-    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
-        return new HelloResponseDto(name,amount);
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
+
+
 }
